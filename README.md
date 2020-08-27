041055795323c6adcb209c90080cf0854521d086
Justine Matsalla <justinem@evanshunt.com>
2020-08-27 15:09:00 -0600

    set up next, react and contentful hookup, install various packages

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # evanshunt-next
+
+This is the codebase for the Evans Hunt website.
+
+## Requirements
+
+* Node.js >= 12.18.3
+* Next >= 9.5.2
+* Contentful >= 7.14.6
+* All other requirements in `package.json`
+
+## Local setup
+
+* Clone this repo
+* Copy `.env.sample` to `.env` and place the Contentful API key credentials
+* Install all dependencies:
+    `docker run --rm -it -v "$(pwd)":/usr/src/app -w /usr/src/app node:dubnium npm install`
+* Run a local webserver that auto-compiles:
+    `docker run --rm -it -p 3000:3000 -v "$(pwd)":/usr/src/app -w /usr/src/app node:dubnium npm run dev`
+* Export a static copy of the site to `/out`:
+    `docker run --rm -it -v "$(pwd)":/usr/src/app -w /usr/src/app node:dubnium npm run build`
+    `docker run --rm -it -v "$(pwd)":/usr/src/app -w /usr/src/app node:dubnium npm run export`
+* To view the static output:
+    `docker run --rm -dit -p 80:80 -v "$(pwd)/out":/usr/local/apache2/htdocs/ httpd:2.4`
+
+## Contributing, feature branch rules
+
+TBD
+
+## Deployment processes
+
+TBD
+
+## QA processes
+
+TBD
+
+## Third-party integrations
+
+* Contentful is used for a headless CMS. Log in with `devs@evanshunt.com` with the credentials in LastPass.
+
+## Gotchas/trickies
+
+TBD
+
+## Production notes
+
+TBD
+
+## Production maintenance
+
+TBD
+
+## Team, contacts
+
+* PM: Sabrina Wilson
+* Clients: Dan Evans & Bill Hunt
+* Tech Lead: Derek McBurney
+* Lead Dev: Justine Matsalla
+* QA: Neetu Verma
+
+## Changelog
+* 18 Aug 2020 Initial repo setup
---
+++ next.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const Dotenv = require('dotenv');
+const DotenvWebPack = require('dotenv-webpack');
+const withFonts = require('nextjs-fonts');
+const withSass = require('@zeit/next-sass');
+const CMSApi = require('./utility/cms');
+
+const next_config = withSass(withFonts({
+  trailingSlash: true,
+  exportPathMap: async function() {
+    const paths = {
+      '/': { page: '/' }
+    };
+
+    // COPIED FROM STAMPEDE FOR REFERENCE
+    // Pull down all applicable routes from Contentful
+    // so static site exports can be supported
+    // Dotenv.config();
+    // const api = new CMSApi();
+    // const pages = await api.fetchContentPages('contentPage');
+    // pages.forEach(page => {
+    //   console.log(page.fields.pageMeta.fields.slug);
+    //   paths[`/${page.fields.pageMeta.fields.slug}`] = { page: '/[slug]', query: { slug: page.fields.pageMeta.fields.slug } };
+    // });
+
+    // const artistPages = await api.fetchContentPages('artistPage');
+    // artistPages.forEach(page => {
+    //   console.log(page.fields.pageMeta.fields.slug);
+    //   paths[`/${page.fields.pageMeta.fields.slug}`] = { page: '/[slug]', query: { slug: page.fields.pageMeta.fields.slug } };
+    // });
+
+    // const venuePages = await api.fetchContentPages('venuePage');
+    // venuePages.forEach(page => {
+    //   console.log(page.fields.pageMeta.fields.slug);
+    //   paths[`/${page.fields.pageMeta.fields.slug}`] = { page: '/[slug]', query: { slug: page.fields.pageMeta.fields.slug } };
+    // });
+
+    return paths;
+  },
+  webpack: config => {
+    config.plugins = config.plugins || [];
+
+    config.plugins = [
+      ...config.plugins,
+      // Read the .env file
+      new DotenvWebPack({
+        path: path.join(__dirname, '.env'),
+        systemvars: true
+      })
+    ];
+
+    return config;
+  },
+}));
+
+module.exports = {...next_config};
