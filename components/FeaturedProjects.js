b331f24bdd44017fd7392f1f1a4d4e47b8f5d6ab
mrobinson537 <matthewr@evanshunt.com>
2020-09-25 12:06:40 -0600

    And some responsive styles for the featured projects.

--- components/FeaturedProjects.js
+++ components/FeaturedProjects.js
@@ -6,13 +6,16 @@ const FeaturedProjects = (props) => {
   
   return (
     <section className="featured-projects">
-      <p><strong>Featured Projects</strong></p>
-      <div className="featured-projects-list">
-        <img src="//via.placeholder.com/1325x714/FF3E3E/FFFFFF?text=Featured+Project" alt="" className="img-fluid" />
-        <img src="//via.placeholder.com/1325x714/333333/FFFFFF?text=Featured+Project" alt="" className="img-fluid" />
-      </div>
-      <div className="featured-projects-btn">
-        <a href="/" className="btn">View All Work</a>
+      <div className="featured-projects-inner">
+        <p><strong>Featured Projects</strong></p>
+        <div className="featured-projects-list">
+          <img src="//via.placeholder.com/1325x714/FF3E3E/FFFFFF?text=Featured+Project" alt="" className="img-fluid" />
+          <img src="//via.placeholder.com/1325x714/333333/FFFFFF?text=Featured+Project" alt="" className="img-fluid" />
+          <a href="/" className="btn featured-projects-btn-2">View All Work</a>
+        </div>
+        {/*<div className="featured-projects-btn">*/}
+        {/*  <a href="/" className="btn">View All Work</a>*/}
+        {/*</div>*/}
       </div>
     </section>
   )
