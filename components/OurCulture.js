e809265342b8a48f24d693441e93086096c2d445
mrobinson537 <matthewr@evanshunt.com>
2020-11-05 14:46:14 -0700

    Improve video functionality on Our Culture component.

--- components/OurCulture.js
+++ components/OurCulture.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import classNames from 'classnames'
 
 class OurCulture extends React.Component {
   
   constructor(props) {
     super(props);
     this.state = {
-      playButton: 'Play'
+      showPlayButton: true
     }
     
     this.handlePlayButton = this.handlePlayButton.bind(this)
+    this.handleVideoEnd = this.handleVideoEnd.bind(this)
   }
 
   // Custom Play/Pause button functionality 
@@ -16,15 +18,21 @@ class OurCulture extends React.Component {
     if (this.video.paused || this.video.ended) {
       this.video.play();
       this.setState({
-        playButton: 'Pause'
+        showPlayButton: false
       })
     }
     else {
       this.video.pause();
       this.setState({
-        playButton: 'Play'
+        showPlayButton: true
       })
     }
+  }
+  
+  handleVideoEnd() {
+    this.setState({
+      showPlayButton: true
+    })
   }
   
   render() {
@@ -32,6 +40,8 @@ class OurCulture extends React.Component {
     const { title, video, images } = this.props
     
     const [img1,img2,img3,img4] = images // not sure how this would work with more or less than the 4 images
+    
+    const playButtonClasses = classNames('play-btn', 'img-fluid', {'hidden': !this.state.showPlayButton})
     
     return(
       <section className="our-culture">
@@ -41,13 +51,15 @@ class OurCulture extends React.Component {
             {img1 && <img src={img1.fields.file.url} alt={img1.fields.file.url} className="img-fluid" />}
             {img2 && <img src={img2.fields.file.url} alt={img2.fields.file.url} className="img-fluid" />}
           </div>
-          <div className="our-culture-column our-culture-column-2">
-            <video ref={video => (this.video = video)} className="video-fluid">
+          <div className="our-culture-column our-culture-column-2" onClick={this.handlePlayButton}>
+            <video ref={video => (this.video = video)} className="video-fluid" onEnded={this.handleVideoEnd}>
               <source src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4" type="video/mp4" />
               Your browser does not support video tags.
             </video>
             <div className="our-culture-play">
-              <button className="our-culture-play-btn" onClick={this.handlePlayButton}>{this.state.playButton}</button>
+              <button className="our-culture-play-btn">
+                <img src="/images/instagram-play.png" alt="play button" height="100" width="100" className={playButtonClasses} />
+              </button>
             </div>
           </div>
           <div className="our-culture-column our-culture-column-3">
