47c732942a2086cce42b2aefed20185bf07c1b4f
Kalen McConnell <kalenm@evanshunt.com>
2020-11-12 17:05:38 -0700

    Adjustments to inline-video block
    - set max-height on the video
    - swap play/pause icons on click
    TODO - probably some responsive adjustments, no animation on the button yet.

--- components/InlineVideo.js
+++ components/InlineVideo.js
@@ -39,7 +39,19 @@ class InlineVideo extends React.Component {
         </video>
 
         <div className="inline-video-control">
-          <button className="inline-video-play" onClick={this.handlePlayButton}>{this.state.playButton}</button>
+          <button className="inline-video-play" onClick={this.handlePlayButton}>
+            {this.state.playButton == 'Play' &&
+            <svg id="play-video" data-name="play-video" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 30">
+              <path id="Path" className="cls-1" d="M1.34,0A1.45,1.45,0,0,0,0,1.44V28.57A1.47,1.47,0,0,0,1.5,30a1.56,1.56,0,0,0,.84-.25l21-13.56a1.39,1.39,0,0,0,.4-2,1.45,1.45,0,0,0-.4-.38L2.34.25a1.53,1.53,0,0,0-1-.24" transform="translate(0 0)"/>
+            </svg>
+            }
+            {this.state.playButton == 'Pause' &&
+              <svg id="pause-video" data-name="pause-video" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 30">
+                <line id="Line-2" className="cls-1" x1="22" y1="3" x2="22" y2="27"/>
+                <line id="Line-2-2" data-name="Line-2" className="cls-1" x1="3" y1="3" x2="3" y2="27"/>
+              </svg>
+            }
+          </button>
           {title && <h3 className="inline-video-title base-font">{title}</h3>}
         </div>
       </div>
