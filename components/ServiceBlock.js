78b650328a9a3f3a29064c22a33c9f3d87bde746
Kalen McConnell <kalenm@evanshunt.com>
2020-11-30 17:53:58 -0700

    Adding additional animation for services images
    - starting to work on responsive tweaks for layout.

--- components/ServiceBlock.js
+++ components/ServiceBlock.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Link from 'next/link'
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 const ServiceBlock = (props) => {
-
   const { textLeftOrRight } = props
   const serviceClass = props.title.toString().toLowerCase()
     .replace(/\s+/g, '-') // Replace spaces with -
@@ -69,22 +71,52 @@ const ServiceBlockText = (props) => {
 
 const ServiceBlockImage = (props) => {
   const { imageBackground, imageForegroundRight, imageForegroundLeft} = props
+  let imgTrigger = useRef(null);
+  let imgBg = useRef(null);
+  let imgLeft = useRef(null);
+  let imgRight = useRef(null);
+
+  useEffect(() => {
+    if (typeof window !== `undefined`) {
+      gsap.registerPlugin(ScrollTrigger);
+      gsap.core.globals("ScrollTrigger", ScrollTrigger);
+    }
+
+    if (imgTrigger.current != null) {
+      const tl = gsap.timeline({defaults: {duration: 10, ease:'none' },
+        paused: true,
+        scrollTrigger: {
+          trigger: imgTrigger.current,
+          scrub: false,
+          start: 'top center',
+          toggleActions: 'play none none reverse',
+          markers: false
+        }
+      });
+
+      tl.fromTo(imgBg.current, {opacity: 0, y: 50}, { duration: 1, opacity: 1, y: 0, ease: 'power4.out' }, "bgIn")
+        .fromTo(imgRight.current, {opacity: 0, x: 50}, { duration: 0.8, opacity: 1, x: 0, ease: 'power4.out' }, "bgIn+=0.5")
+        .fromTo(imgLeft.current, {opacity: 0, x: -50}, { duration: 0.8, opacity: 1, x: 0, ease: 'power4.out'}, "bgIn+=1");
+    }
+
+  }, [])
+
+
   return (
-    <div className="service-block-img">
-      {/*<img src={image.fields.file.url} className="img-fluid" alt={image.fields.file.description} />*/}
+    <div ref={imgTrigger} className="service-block-img">
       {imageBackground && (
         <div className="img-background">
-          <img src={imageBackground.fields.file.url} className="img-fluid" alt={imageBackground.fields.file.description} />
+          <img ref={imgBg} src={imageBackground.fields.file.url} className="img-fluid" alt={imageBackground.fields.file.description} />
         </div>
       )}
       {imageForegroundRight && (
         <div className="img-right-side">
-          <img src={imageForegroundRight.fields.file.url} className="img-fluid" alt={imageForegroundRight.fields.file.description} />
+          <img ref={imgRight} src={imageForegroundRight.fields.file.url} className="img-fluid" alt={imageForegroundRight.fields.file.description} />
         </div>
       )}
       {imageForegroundLeft && (
         <div className="img-left-side">
-          <img src={imageForegroundLeft.fields.file.url} className="img-fluid" alt={imageForegroundLeft.fields.file.description} />
+          <img ref={imgLeft} src={imageForegroundLeft.fields.file.url} className="img-fluid" alt={imageForegroundLeft.fields.file.description} />
         </div>
       )}
     </div>
