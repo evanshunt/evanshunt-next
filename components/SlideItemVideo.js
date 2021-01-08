9a8bc52b5dd178aee63857dbef61196016d4cd0f
Kalen McConnell <kalenm@evanshunt.com>
2021-01-08 16:13:14 -0700

    Rejigging the modal
    Wasn’t super happy with the other modal implementation I did, couldn’t have transitions and couldn’t add a class to the modal when it was added to the dom.

--- components/SlideItemVideo.js
+++ components/SlideItemVideo.js
@@ -1,65 +1,93 @@
-import React from "react";
+import React from 'react'
 import { Image } from "pure-react-carousel";
-import { PortalWithState } from 'react-portal';
+import Modal from 'react-modal'
 
-//import Link from "next/link";
+Modal.setAppElement('#__next')
 
 class SlideItemVideo extends React.Component {
+
   constructor(props) {
     super(props);
+    this.modalWrapper = null;
+    this.state = {
+      setIsOpen: false
+    };
+
+    this.openModal = this.openModal.bind(this)
+    this.closeModal = this.closeModal.bind(this)
+
+  }
+
+  openModal() {
+    this.setState({ setIsOpen: true });
+  }
+
+  afterOpenModal() {
+    document.body.style.top = `-${window.scrollY}px`
+    document.body.style.position = 'fixed'
+  }
+
+  onAfterClose() {
+    const scrollY = document.body.style.top
+    document.body.style.position = ''
+    document.body.style.top = ''
+    window.scrollTo(0, parseInt(scrollY || '0') * -1)
+  }
+
+  closeModal(){
+    this.setState({ setIsOpen: false });
   }
 
   render() {
-    console.log(this);
     const { title, image, videoPreview, videoDownload } = this.props;
 
     return (
       <div className="slide-item-video-wrapper">
-      <PortalWithState closeOnOutsideClick closeOnEsc>
-        {({ openPortal, closePortal, isOpen, portal }) => (
-          <React.Fragment>
-            <div className="slide-item-video">
-              <div className="text-on-image">
-                <div className="image-wrapper">
-                  <Image
-                    src={image.file.url}
-                    className="img-fluid"
-                    alt={image.file.description}
-                    onClick={openPortal}
-                  />
-                </div>
-                <div className="text-wrapper">
-                  {title && <h6 className="slide-title">{title}</h6>}
-                </div>
-              </div>
+        <div className="slide-item-video">
+          <div className="text-on-image">
+            <div className="image-wrapper">
+              <Image
+                src={image.file.url}
+                className="img-fluid"
+                alt={image.file.description}
+                onClick={this.openModal}
+              />
             </div>
-            {portal(
-              <div className={isOpen ? 'modal open' : 'modal'}>
-                <div className="modal-content">
-                  {title && <h6 className="slide-title">{title}</h6>}
-                  {videoPreview &&
-                    <div className="embed-responsive embed-responsive-16by9">
-                        <video controls>
-                          <source src={videoPreview.fields.file.url} type="video/mp4" />
-                        </video>
-                    </div>
-                  }
-                  {videoDownload &&
-                    <div className="download-video-link">
-                       <a href={videoDownload.fields.file.url} target="_blank" rel="noreferrer">
-                       Download Full Video here
-                        </a>
-                      </div>
-                  }
-                  <button onClick={closePortal}>Close me!</button>
+            <div className="text-wrapper">
+              {title && <h6 className="slide-title">{title}</h6>}
+            </div>
+          </div>
+        </div>
+        <Modal
+          isOpen={this.state.setIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onAfterClose={this.onAfterClose}
+          onRequestClose={this.closeModal}
+          contentLabel={title}
+          className="modal"
+          overlayClassName="modal-overlay"
+        >
+          <div className="modal-content">
+            {title && <h6 className="slide-title">{title}</h6>}
+              {videoPreview &&
+                <div className="embed-responsive embed-responsive-16by9">
+                  <video controls>
+                    <source src={videoPreview.fields.file.url} type="video/mp4" />
+                  </video>
                 </div>
-              </div>
-            )}
-          </React.Fragment>
-        )}
-      </PortalWithState>
-    </div>
-
+              }
+              {videoDownload &&
+                <div className="download-video-link">
+                  <a className="external-link" href={videoDownload.fields.file.url} target="_blank" rel="noreferrer">
+                    Download Full Video here
+                    <img className="external-link-icon" src="/images/external-link.svg" alt="external link icon"/>
+                  </a>
+                </div>
+              }
+              <button onClick={this.closeModal}>close</button>
+            </div>
+        </Modal>
+      </div>
     );
   }
 }
