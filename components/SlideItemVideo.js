5ae43e5906efb400b46c2dbe9d6d1c93c789e579
Kalen McConnell <kalenm@evanshunt.com>
2021-01-07 17:24:41 -0700

    Extremely rough modal work

--- components/SlideItemVideo.js
+++ components/SlideItemVideo.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Image } from "pure-react-carousel";
+import { PortalWithState } from 'react-portal';
+
 //import Link from "next/link";
 
 class SlideItemVideo extends React.Component {
@@ -8,24 +10,56 @@ class SlideItemVideo extends React.Component {
   }
 
   render() {
-    //console.log(this);
-    const { title, image } = this.props;
+    console.log(this);
+    const { title, image, videoPreview, videoDownload } = this.props;
 
     return (
-      <div className="slide-item-video">
-        <div className="text-on-image">
-          <div className="image-wrapper">
-            <Image
-              src={image.file.url}
-              className="img-fluid"
-              alt={image.file.description}
-            />
-          </div>
-          <div className="text-wrapper">
-            {title && <h6 className="slide-title">{title}</h6>}
-          </div>
-        </div>
-      </div>
+      <div className="slide-item-video-wrapper">
+      <PortalWithState closeOnOutsideClick closeOnEsc>
+        {({ openPortal, closePortal, isOpen, portal }) => (
+          <React.Fragment>
+            <div className="slide-item-video">
+              <div className="text-on-image">
+                <div className="image-wrapper">
+                  <Image
+                    src={image.file.url}
+                    className="img-fluid"
+                    alt={image.file.description}
+                    onClick={openPortal}
+                  />
+                </div>
+                <div className="text-wrapper">
+                  {title && <h6 className="slide-title">{title}</h6>}
+                </div>
+              </div>
+            </div>
+            {portal(
+              <div className={isOpen ? 'modal open' : 'modal'}>
+                <div className="modal-content">
+                  {title && <h6 className="slide-title">{title}</h6>}
+                  {videoPreview &&
+                    <div className="embed-responsive embed-responsive-16by9">
+                        <video controls>
+                          <source src={videoPreview.fields.file.url} type="video/mp4" />
+                        </video>
+                    </div>
+                  }
+                  {videoDownload &&
+                    <div className="download-video-link">
+                       <a href={videoDownload.fields.file.url} target="_blank" rel="noreferrer">
+                       Download Full Video here
+                        </a>
+                      </div>
+                  }
+                  <button onClick={closePortal}>Close me!</button>
+                </div>
+              </div>
+            )}
+          </React.Fragment>
+        )}
+      </PortalWithState>
+    </div>
+
     );
   }
 }
