e8628f62e40ea84de137cdf40eb83145f839e513
dmcb <d.mcburney@icloud.com>
2022-11-18 09:55:49 -0700

    Adds circle charts

--- components/DEIResult.js
+++ components/DEIResult.js
@@ -2,18 +2,30 @@ import React from "react";
 
 const DEIResult = ({ question, results, activeResultYear, barChart }) => {
   if (results[activeResultYear]) {
-    return (
-      <li>
-        {!barChart && <strong>Yes</strong>}
-        <p>{question}</p>
-        <div className="value-visual">
-          <div className="quantity" style={{width: `${results[activeResultYear]}%`}}></div>
-          <span className="value">
-            {results[activeResultYear]}%
-          </span>
-        </div>
-      </li>
-    );
+    if (!barChart)
+      return (
+        <li>
+          <p><strong>Yes</strong>{question}</p>
+          <div className="value-visual">
+            <div className="quantity" style={{background: `conic-gradient(white ${100-results[activeResultYear]}%, red ${100-results[activeResultYear]}%)`}}></div>
+            <span className="value">
+              {results[activeResultYear]}%
+            </span>
+          </div>
+        </li>
+      );
+    else 
+      return (
+        <li>
+          <p>{question}</p>
+          <div className="value-visual">
+            <div className="quantity" style={{width: `${results[activeResultYear]}%`}}></div>
+            <span className="value">
+              {results[activeResultYear]}%
+            </span>
+          </div>
+        </li>
+      );
   }
   else return null;
 };
